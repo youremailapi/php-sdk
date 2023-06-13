fc4ee028fb5cd82ea7bec44600d90f0daf724271
Federico Juretich <fedejuret@gmail.com>
2023-06-13 11:41:16 -0300

    update SendRequestTest class

---
+++ tests/Constants.php
@@ -0,0 +1,11 @@
+<?php
+
+class Constants
+{
+
+    const DEFAULT_TO_EMAIL = 'noreply@youremailapi.com';
+    const DEFAULT_TEMPLATE = 'ded580ac7802f8e0b55adcb4b0ee528ebdabe3b544f66262875fc7e85b4ec623';
+    const DEFAULT_SMTP_ACCOUNT = '593065f3f704ff3a1d881fbd5ffb73478ced87daf77b3064981a28d3f421b4a3';
+    const APIKEY = 'acf5c2f932bf855723fe68157e20db636bd28713b5a28f475565d0a2dac76b46';
+
+}
